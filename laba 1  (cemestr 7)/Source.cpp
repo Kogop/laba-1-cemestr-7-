83652c7af4c7f9958fd6788ed7b774746cd296af
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-09-10 16:57:41 +0300

    addd

--- laba 1  (cemestr 7)/Source.cpp
+++ laba 1  (cemestr 7)/Source.cpp
@@ -5,90 +5,99 @@ const double pi = atan(1) * 4;
 int roR = 1380, t0 = 6, roV = 1000, mu = 0.001, Senm = 0;
 double nu = 0.09, Q = 0.038, D = 0.222, dn1 = 0.144, dv1 = 0.126, L1 = 2830, dn2 = 0.178, dv2 = 0.6, L2 = 60;
 
-double Vsr() {
-	return 4 * Q / (pi * dv1);
+double Vsr(double x) {
+	return 4 * Q / (pi * x);
 
 
 }
-double SenM() {
-	return t0 * dv1 / (nu * Vsr());
+double SenM(double x) {
+	return t0 * x / (nu * Vsr(x));
 
 }
-double Rem() {
-	return roR * Vsr() * dv1 / (nu * (1 + (1 / 6.0) * SenM()));
+double Rem(double x) {
+	return roR * Vsr(x) * x / (nu * (1 + (1 / 6.0) * SenM(x)));
 
 
 }
 
-double Lambda() {
+double Lambda(double x) {
 
-	if (Rem()<2100)
+	if (Rem(x)<2100)
 	{
-		return 64 / Rem();
+		return 64 / Rem(x);
 
 	}
 	else
 	{
-		return 0.316 / pow(Rem(), 0.25);
+		return 0.316 / pow(Rem(x), 0.25);
 	}
 
 }
-double dPm() {
+double dPm(double x) {
 
-	return Lambda() * (L1 / dv1) * ((roR * pow(Vsr(), 2)) / 2) / 100000;
-
-}
-
-double VsrN() {
-
-
-
-	return 4 * Q / (pi * (D * D - dn1 * dn1));
-
-
-}
-double SenMN() {
-	return t0 * (D * D - dn1 * dn1) / (nu * VsrN());
-
-}
-double RemN() {
-	return roR * VsrN() * (D - dn1) / (nu * (1 + (1 / 8.0) * SenMN()));
-
+	return Lambda(x) * (L1 / x) * ((roR * pow(Vsr(x), 2)) / 2) / 100000;
 
 }
 
-double LambdaN() {
+double VsrN(double y) {
 
-	if (Rem() < 1600)
+
+
+	return 4 * Q / (pi * (D * D - y * y));
+
+
+}
+double SenMN(double y) {
+	return t0 * (D * D - y * y) / (nu * VsrN(y));
+
+}
+double RemN(double y) {
+	return roR * VsrN(y) * (D - y) / (nu * (1 + (1 / 8.0) * SenMN(y)));
+
+
+}
+
+double LambdaN(double y) {
+
+	if (Rem(y) < 1600)
 	{
-		return 96 / RemN();
+		return 96 / RemN(y);
 
 	}
 	else
 	{
-		return 0.316 / pow(RemN(), 0.25);
+		return 0.316 / pow(RemN(y), 0.25);
 	}
 
 }
-double dPmN() {
+double dPmN(double y) {
 
-	return LambdaN() * (L1 / (D - dn1)) * ((roR * pow(VsrN(), 2)) / 2) / 100000;
+	return LambdaN(y) * (L1 / (D - y)) * ((roR * pow(VsrN(y), 2)) / 2) / 100000;
 
 }
-double DP() {
+double DP(double x, double y) {
 
-	return dPm() + dPmN();
+	return dPm(x) + dPmN(y);
 	
 }
 
-double N() {
+double N(double x,double y) {
 
-	return DP() * Q / nu;
+	return DP(x,y) * Q / (0.4 / 0.6);
 }
 
+double Voda() {
+	double RemV = roV * Vsr(dv1) * dv1 / nu;
+	double RemNV = roV;
+}
+
+
+
+
 int main() {
-
-	cout << N();
-
+	
+	cout << N(dv1, dn1);
+	N();
+		
 	return 1;
 }
